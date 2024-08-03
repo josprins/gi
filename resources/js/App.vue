6c565a386b8d842d4ddb94b8b1c0ebe3d48ad980
Jos Prins <jco.prins@gmail.com>
2024-08-03 20:06:29 +0200

    First Commit: Laravel and Vue boilerplate

---
+++ resources/js/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+      <!-- Render the ExampleComponent correctly -->
+      <ExampleComponent msg="my message"/>
+    </div>
+  </template>
+  
+  <script setup>
+  import ExampleComponent from "./components/ExampleComponent.vue";
+  </script>
+  
+  <style>
+  /* Add your styles here */
+  </style>
---
+++ resources/js/app.js
@@ -0,0 +1,6 @@
+import './bootstrap';
+import { createApp } from 'vue/dist/vue.esm-bundler.js';
+import App from "./App.vue"
+
+createApp(App).mount("#app")
+
