05a02d31d026e1b235dfad672837e65048b7db6f
Jos Prins <jco.prins@gmail.com>
2024-08-11 17:09:41 +0200

    installed pinia

--- resources/js/components/pages/home/HomeComponent.vue
+++ resources/js/components/pages/home/HomeComponent.vue
@@ -1,6 +1,17 @@
 <template>
-    <div>
-        HOME
-    </div>
+  <div>
+    <div>COUNTER = {{ count }}</div>
+    <button class="btn btn-primary" @click="increment">+</button>
+    <button class="btn btn-primary mx-1" @click="decrement">-</button>
+  </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { exampleStore } from '../../../stores/example.js';
+import { storeToRefs } from 'pinia';
+
+const counterStore = exampleStore();
+
+// Destructure state and actions using storeToRefs
+const { count } = storeToRefs(counterStore);
+const { increment, decrement } = counterStore;
+</script>
